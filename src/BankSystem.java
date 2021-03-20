2b327132b85ee439107a68d684727d0347e1c81e
Luiz Mariz <lfsmariz@gmail.com>
2021-03-20 16:39:22 -0300

    main

---
+++ src/BankSystem.java
@@ -0,0 +1,13 @@
+import user.Comprador;
+
+/**
+ * BankSystem
+ */
+public class BankSystem {
+  public static void main(String[] args) {
+
+    Comprador usuario1 = new Comprador("luiz", 15, "123");
+
+    System.out.println(usuario1);
+  }
+}
